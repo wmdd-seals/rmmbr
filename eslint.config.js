0c32c7761beb3b41bcaed9be30e92000efb2b66c
rasulomaroff <rasulomaroff@gmail.com>
2024-05-28 21:07:04 -0700

    lint: adds linting

---
+++ eslint.config.js
@@ -0,0 +1,10 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default [
+  { ignores: ["**/*.js"] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+];
