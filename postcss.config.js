6258ab9b11561ca04036e4cd1737728eb31067fb
rasulomaroff <rasulomaroff@gmail.com>
2024-05-31 00:02:01 -0700

    feat: adds tailwind integration

---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+import autoprefixer from 'autoprefixer'
+import tailwindcss from 'tailwindcss'
+
+export default {
+    /* Postcss plugins should come first */
+    plugins: [autoprefixer, tailwindcss]
+}
