17246ab7eb9672eda5c2003bbd8446bdca308077
tom555-555 <tomok.development@gmail.com>
2024-05-30 21:45:28 -0700

    feat: removed unnecesary lines from main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,1 @@
-import { firebaseApp } from './helpers/firebase.js'
-
-import { getFirestore } from 'firebase/firestore'
-import { getAuth } from 'firebase/auth'
-
-const firestore = getFirestore(firebaseApp)
-const auth = getAuth(firebaseApp)
-
-console.log({ firestore, auth })
+console.log('it works!')
