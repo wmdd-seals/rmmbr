655601f32fab2b4f5cf9add46d8903121e1c75aa
rasulomaroff <rasulomaroff@gmail.com>
2024-06-03 19:41:21 -0700

    dev: includes `explicit-return` rule to eslint

--- eslint.config.js
+++ eslint.config.js
@@ -4,9 +4,24 @@ import tseslint from 'typescript-eslint'
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 
 export default [
-    { ignores: ['**/*.js'] },
+    {
+        ignores: ['**/*.js']
+    },
     { languageOptions: { globals: globals.browser } },
     pluginJs.configs.recommended,
     ...tseslint.configs.recommended,
+    ...tseslint.configs.recommendedTypeChecked,
+    ...tseslint.config({
+        plugins: { '@typescript-eslint': tseslint.plugin },
+        languageOptions: {
+            parser: tseslint.parser,
+            parserOptions: {
+                project: true
+            }
+        },
+        rules: {
+            '@typescript-eslint/explicit-function-return-type': 'error'
+        }
+    }),
     eslintPluginPrettierRecommended
 ]
