05605a2eb896f3211b750b48815b05ed448ccdd7
rasulomaroff <rasulomaroff@gmail.com>
2024-06-03 19:36:34 -0700

    dev: adds types for env file

---
+++ @types/env.d.ts
@@ -0,0 +1,12 @@
+interface ImportMeta {
+    env: ImportMetaEnv
+}
+
+interface ImportMetaEnv {
+    VITE_FIREBASE_API_KEY: string
+    VITE_FIREBASE_AUTH_DOMAIN: string
+    VITE_FIREBASE_PROJECT_ID: string
+    VITE_FIREBASE_STORAGE_BUCKET: string
+    VITE_FIREBASE_MESSAGING_SENDER_ID: string
+    VITE_FIREBASE_APP_ID: string
+}
