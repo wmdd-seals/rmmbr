41ef6c80c28e74f56b90243cf4cf66ab57ab7a9e
rasulomaroff <rasulomaroff@gmail.com>
2024-06-09 01:26:21 -0700

    refactor: changes entry point to src/pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,31 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import BasicSSL from '@vitejs/plugin-basic-ssl'
+import { resolve } from 'path'
 
 export default defineConfig({
     server: {
         open: true,
         https: {}
     },
-    base: '/rmmbr/',
+    // for github, we build it in /rmmbr directory, so that it was accessible
+    // under *.github.io/rmmbr/
+    base: process.env.VITE_DESTINATION === 'github' ? '/rmmbr/' : '/',
+    root: resolve(__dirname, 'src/pages'),
+    envDir: __dirname,
+    publicDir: resolve(__dirname, 'public'),
     plugins: [BasicSSL()],
+    build: {
+        outDir: resolve(__dirname, 'dist'),
+        emptyOutDir: true,
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'src/pages/index.html')
+                // memory: resolve(__dirname, 'src/pages/memory/index.html')
+                // me: resolve(__dirname, 'src/pages/me/index.html')
+            }
+        }
+    },
     test: {
         globals: true,
         environment: 'happy-dom'
