c110e2445005702a762144d056e4cee3e36a4d01
tom555-555 <tomok.development@gmail.com>
2024-06-12 11:34:45 -0700

    api: chagned the argument to initialize auth instance

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -8,8 +8,10 @@ type loginCred = {
 }
 
 class Auth {
-    constructor(private _currentUser: User | null) {
-        this._currentUser = _currentUser
+    private _currentUser: User | null
+    constructor(private _auth: typeof auth) {
+        this._currentUser = _auth.currentUser
+        this._auth = _auth
     }
 
     // getter
@@ -19,7 +21,7 @@ class Auth {
 
     async signUpwithEmailAndPassword(loginCred: loginCred): Promise<void | FirebaseError> {
         try {
-            const { user } = await createUserWithEmailAndPassword(auth, loginCred.email, loginCred.password)
+            const { user } = await createUserWithEmailAndPassword(this._auth, loginCred.email, loginCred.password)
             this._currentUser = user
         } catch (err) {
             return err as FirebaseError
@@ -28,7 +30,7 @@ class Auth {
 
     async logoutFirebase(): Promise<void | FirebaseError> {
         try {
-            await signOut(auth)
+            await signOut(this._auth)
             this._currentUser = null
             return
         } catch (err) {
@@ -38,7 +40,7 @@ class Auth {
 
     async loginWithEmailAndPassword(loginCred: loginCred): Promise<void | FirebaseError> {
         try {
-            const { user } = await signInWithEmailAndPassword(auth, loginCred.email, loginCred.password)
+            const { user } = await signInWithEmailAndPassword(this._auth, loginCred.email, loginCred.password)
             this._currentUser = user
         } catch (err) {
             return err as FirebaseError
@@ -46,4 +48,4 @@ class Auth {
     }
 }
 
-export const account = new Auth(auth.currentUser)
+export const account = new Auth(auth)
