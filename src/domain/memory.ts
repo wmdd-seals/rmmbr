c80a74c339a6e04fb46cb00d49bfc993777c587a
rasulomaroff <rasulomaroff@gmail.com>
2024-06-12 11:48:26 -0700

    feat: adds `Memory` entity definition

--- src/domain/memory.ts
+++ src/domain/memory.ts
@@ -1,5 +1,12 @@
-import { ID } from '../utils'
+import { ID, Location, Maybe } from '../utils'
 
 export abstract class Memory {
     public abstract id: ID<string, 'memory-id'>
+    public abstract title: string
+    public abstract description: Maybe<string>
+    public abstract date: string
+    public abstract location: Maybe<Location>
+    public abstract viewerIds: string[]
+    public abstract editorIds: string[]
+    public abstract ownerId: string
 }
