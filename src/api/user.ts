026a71b0a1c29ea940d9d0a4d20085f2d5740e37
tom555-555 <tomok.development@gmail.com>
2024-06-12 21:37:04 -0700

    api: removed unnecessary lines

--- src/api/user.ts
+++ src/api/user.ts
@@ -7,34 +7,21 @@ type LoginCred = {
     password: string
 }
 
-type Options = {
-    onSuccess?: () => void
-}
-
 class UserApi {
     constructor(private _auth: Auth) {
         this._auth = _auth
     }
 
-    async signUp(loginCred: LoginCred, options?: Options): Promise<void | FirebaseError> {
+    async signUp(loginCred: LoginCred): Promise<void | FirebaseError> {
         await createUserWithEmailAndPassword(this._auth, loginCred.email, loginCred.password)
-        if (options && options.onSuccess) {
-            options.onSuccess()
-        }
     }
 
-    async signOut(options?: Options): Promise<void> {
+    async signOut(): Promise<void> {
         await signOut(this._auth)
-        if (options && options.onSuccess) {
-            options.onSuccess()
-        }
     }
 
-    async signIn(loginCred: LoginCred, options?: Options): Promise<void> {
+    async signIn(loginCred: LoginCred): Promise<void> {
         await signInWithEmailAndPassword(this._auth, loginCred.email, loginCred.password)
-        if (options && options.onSuccess) {
-            options.onSuccess()
-        }
     }
 }
 
