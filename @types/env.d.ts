dbb4da4c10eb762c15ff145224131635b2ad72a7
tom555-555 <tomok.development@gmail.com>
2024-06-15 19:55:52 -0700

    setup: removed firebase

--- @types/env.d.ts
+++ @types/env.d.ts
@@ -3,11 +3,6 @@ interface ImportMeta {
 }
 
 interface ImportMetaEnv {
-    VITE_FIREBASE_API_KEY: string
-    VITE_FIREBASE_AUTH_DOMAIN: string
-    VITE_FIREBASE_PROJECT_ID: string
-    VITE_FIREBASE_STORAGE_BUCKET: string
-    VITE_FIREBASE_MESSAGING_SENDER_ID: string
-    VITE_FIREBASE_APP_ID: string
-    VITE_FIREBASE_STORAGE_BUCKET_LINK: string
+    VITE_PUBLIC_SUPABASE_URL: string
+    VITE_PUBLIC_SUPABASE_ANON_KEY: string
 }
