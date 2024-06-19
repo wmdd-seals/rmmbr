ae2e9122e089c93de4f32910bb22770eb2b34e11
tom555-555 <tomok.development@gmail.com>
2024-06-19 00:49:11 -0700

    setup: init google map / enabled to put multiple pins on the map

--- @types/env.d.ts
+++ @types/env.d.ts
@@ -5,4 +5,5 @@ interface ImportMeta {
 interface ImportMetaEnv {
     VITE_PUBLIC_SUPABASE_URL: string
     VITE_PUBLIC_SUPABASE_ANON_KEY: string
+    VITE_PUBLIC_GOOGLE_MAP_API_KEY: string
 }
---
+++ src/utils/gmap.ts
@@ -0,0 +1,54 @@
+import { Loader } from '@googlemaps/js-api-loader'
+
+type LatLng = {
+    lat: number
+    lng: number
+}
+
+const loader = new Loader({
+    apiKey: import.meta.env?.VITE_PUBLIC_GOOGLE_MAP_API_KEY,
+    version: 'weekly',
+    libraries: ['places']
+})
+
+export class GMap {
+    private maps: google.maps.Map | null | undefined = null
+
+    constructor(element: HTMLElement, latLng: LatLng) {
+        void this.initMap(element, latLng)
+    }
+
+    private async initMap(element: HTMLElement, latLng: LatLng): Promise<void> {
+        const mapOptions = {
+            center: latLng,
+            zoom: 15,
+            mapId: 'rmmbr_map'
+        }
+        try {
+            const { Map } = await loader.importLibrary('maps')
+            this.maps = new Map(element, mapOptions)
+        } catch (err) {
+            // todo: error handling
+            console.error(err)
+        }
+    }
+
+    public async putMarker(latLngs: LatLng[]): Promise<void> {
+        console.log(this.maps)
+
+        try {
+            const { AdvancedMarkerElement } = await loader.importLibrary('marker')
+
+            latLngs.forEach(xy => {
+                new AdvancedMarkerElement({
+                    position: xy,
+                    map: this.maps,
+                    title: 'hello'
+                })
+            })
+        } catch (err) {
+            // todo: error handling
+            console.error(err)
+        }
+    }
+}
