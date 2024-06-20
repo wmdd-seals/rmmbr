8fea7d3e1807b2cd1b6894c35871495945a335dc
rasulomaroff <rasulomaroff@gmail.com>
2024-06-20 00:33:07 -0700

    feat: adds ui-kit page together with the first ui elements - buttons and inputs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,35 @@ export default {
     content: ['./index.html', './src/**/*.{ts,html}'],
     theme: {
         extend: {
+            backgroundImage: {
+                cta: 'radial-gradient(818.93% 100% at 100% 50%, #242759 0%, #464BAA 100%)',
+                'cta-hover': 'radial-gradient(818.93% 100% at 100% 50%, #353980 0%, #646ADB 100%)',
+                'cta-active': 'radial-gradient(818.93% 100% at 100% 50%, #242759 0%, #353980 100%)',
+                'cta-focus': 'radial-gradient(818.93% 100% at 100% 50%, #242759 0%, #464BAA 100%)'
+            },
+            boxShadow: {
+                'input-focus': '0px 0px 8px 0px #646ADB',
+                cta: '0px 4px 12px 0px rgba(0, 0, 0, 0.25), 0px -3px 16px 0px #575ED5 inset',
+                'cta-active': '0px 4px 12px 0px rgba(0, 0, 0, 0.25), 0px -3px 16px 0px #575ED5 inset',
+                'cta-focus': '0px -3px 16px 0px #575ED5 inset'
+            },
             colors: {
+                ui: {
+                    50: '#F6F7FA',
+                    100: '#EDEEF5',
+                    200: '#D5D9E5',
+                    300: '#B4BCCC',
+                    400: '#929EB2',
+                    500: '#718299'
+                },
+                type: {
+                    50: '#111433',
+                    100: '#14213B',
+                    200: '#2B3C5F',
+                    300: '#4B6384',
+                    400: '#7389A8',
+                    500: '#A3B4CC'
+                },
                 basketball: {
                     50: '#FDEDE7',
                     100: '#FCE0D4',
