e9bd6e36f93790f2e3c6382aeff1bbaa92ac014c
rasulomaroff <rasulomaroff@gmail.com>
2024-06-28 15:38:11 -0700

    refactor(account-page): simplifies logic

--- src/pages/account/account.ts
+++ src/pages/account/account.ts
@@ -1,109 +1,39 @@
-import { userApi } from '../../api/userApi.ts'
-import { User } from '#domain'
+import { userApi } from '#api'
 
-const avatarInput = document.getElementById('avatar') as HTMLInputElement
+userApi.getCurrent().then(user => {
+    if (!user) return
 
-const saveBtn = document.getElementById('save-btn')!
-const deleteImageBtn1 = document.getElementById('delete-btn1')!
-const deleteImageBtn2 = document.getElementById('delete-btn2')!
-const deleteImgBtns = [deleteImageBtn1, deleteImageBtn2]
+    const { id, firstName, lastName, avatarSrc, email } = user
 
-const avatarImgSm = document.getElementById('avatar-img-sm') as HTMLImageElement
-const avatarImgLg = document.getElementById('avatar-img-lg') as HTMLImageElement
+    const nameDisplay = document.getElementById('name-display')!
+    const firstNameInputField = document.getElementById('first-name') as HTMLInputElement
+    const lastNameInputField = document.getElementById('last-name') as HTMLInputElement
+    const emailInputField = document.getElementById('email') as HTMLInputElement
+    const avatarInput = document.getElementById('avatar') as HTMLInputElement
+    const avatarImgLg = document.getElementById('avatar-img-lg') as HTMLImageElement
 
-const nameDisplay = document.getElementById('name-display')!
-const firstNameInputField = document.getElementById('first-name') as HTMLInputElement
-const lastNameInputField = document.getElementById('last-name') as HTMLInputElement
-const emailInputField = document.getElementById('email') as HTMLInputElement
+    nameDisplay.innerHTML = firstName
+    firstNameInputField.value = firstName
+    lastNameInputField.value = lastName
+    emailInputField.value = email
+    avatarImgLg.src = avatarSrc || ''
 
-let userId: User['id']
-let firstName: User['firstName']
-let lastName: User['lastName']
-let email: User['email']
+    const saveBtn = document.getElementById('save-btn')!
 
-async function fetchUserData(): Promise<void> {
-    const userData = await userApi.getCurrent()
-    if (!userData) {
-        return
-    }
-    userId = userData.id
-    email = userData.email
-    firstName = userData.firstName
-    lastName = userData.lastName
-}
+    saveBtn.addEventListener('click', async () => {
+        const newAvatar = avatarInput.files?.[0]
 
-window.onload = async (): Promise<void> => {
-    await fetchUserData()
-    if (!firstName && !lastName && !email) {
-        return
-    }
-    nameDisplay.textContent = firstName
-    firstNameInputField.placeholder = firstName
-    lastNameInputField.placeholder = lastName
-    emailInputField.placeholder = email
-}
+        if (newAvatar) {
+            void userApi.uploadAvatar(id, newAvatar)
+        }
 
-saveBtn.addEventListener('click', async () => {
-    await fetchUserData()
-    await updateAndDisplayAvatar()
-    await updateAndDisplayName()
-})
+        const updatedUser = await userApi.update(id, {
+            firstName: firstNameInputField.value,
+            lastName: lastNameInputField.value
+        })
 
-async function updateAndDisplayAvatar(): Promise<void> {
-    let file = avatarInput.files?.[0]
-    console.log(file)
+        if (!updatedUser) return
 
-    if (!file) {
-        console.log('error')
-        return
-    }
-    try {
-        await userApi.deleteAvatar(userId)
-        await userApi.uploadAvatar(userId, file)
-
-        userApi.getAvatarUrl(userId, avatarImgSm, deleteImageBtn2)
-        userApi.getAvatarUrl(userId, avatarImgLg, deleteImageBtn1)
-        avatarInput.value = ''
-    } catch (error) {
-        console.error(error)
-    }
-}
-
-async function updateAndDisplayName(): Promise<void> {
-    const firstNameValue = firstNameInputField.value
-    const lastNameValue = lastNameInputField.value
-
-    if (!firstNameValue && !lastNameValue) {
-        return
-    } else if (firstNameValue && lastNameValue) {
-        await userApi.updateName(userId, { firstName: firstNameValue, lastName: lastNameValue })
-        await fetchUserData()
-        nameDisplay.textContent = firstName
-    } else if (firstNameValue && !lastNameValue) {
-        await userApi.updateName(userId, { firstName: firstNameValue })
-        await fetchUserData()
-        nameDisplay.textContent = firstName
-    } else {
-        await userApi.updateName(userId, { lastName: lastNameValue })
-        await fetchUserData()
-        nameDisplay.textContent = firstName
-    }
-}
-
-deleteImgBtns.forEach(btn => {
-    btn.addEventListener('click', async () => {
-        await fetchUserData()
-        await deleteAvatar()
-        deleteImageBtn1.textContent = ''
-        deleteImageBtn2.textContent = ''
+        nameDisplay.innerHTML = updatedUser.firstName
     })
-})
-async function deleteAvatar(): Promise<void> {
-    try {
-        await userApi.deleteAvatar(userId)
-        avatarImgSm.src = ''
-        avatarImgLg.src = ''
-    } catch (error) {
-        console.error(error)
-    }
-}
+}, console.error)
