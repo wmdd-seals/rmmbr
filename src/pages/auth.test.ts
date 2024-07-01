40509d7cc91406708c0fb7805c164c456d2c82b0
rasulomaroff <rasulomaroff@gmail.com>
2024-06-30 17:14:30 -0700

    spec: mocks supabase object

--- src/pages/auth.test.ts
+++ src/pages/auth.test.ts
@@ -5,6 +5,10 @@ import { AuthChangeEvent, Session } from '@supabase/supabase-js'
 
 vi.mock('src/api/supabase.ts', () => ({
     supabase: {
+        from: (): void => void 0,
+        storage: {
+            from: (): void => void 0
+        },
         auth: {
             onAuthStateChange: (callback: (event: AuthChangeEvent, sesstion: Session | null) => void): void => {
                 callback('SIGNED_OUT', null)
