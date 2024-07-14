7833f2e1d1b3e7d0e0e2171369361832ecc41833
tom555-555 <tomok.development@gmail.com>
2024-07-14 14:06:43 -0700

    feat: counts by spans, statics on memory page

--- src/pages/memory/memory.ts
+++ src/pages/memory/memory.ts
@@ -249,6 +249,15 @@ userApi
             })
         }
         selectAndDeleteMoments()
+
+        const countDate = new CountTime(memory.date)
+        q<HTMLSpanElement>('[data-yearly-count]').innerHTML = countDate.yearly()
+        q<HTMLSpanElement>('[data-monthly-count]').innerHTML = countDate.monthly()
+        q<HTMLSpanElement>('[data-weekly-count]').innerHTML = countDate.weekly()
+        q<HTMLSpanElement>('[data-daily-count]').innerHTML = countDate.daily()
+        q<HTMLSpanElement>('[data-hourly-count]').innerHTML = countDate.hourly()
+        q<HTMLSpanElement>('[data-minutes-count]').innerHTML = countDate.minutes()
+        q<HTMLSpanElement>('[data-seconds-count]').innerHTML = countDate.seconds()
     })
     .catch(console.error)
 
@@ -353,7 +362,6 @@ class LatestMoments {
                     filter: `memoryId=eq.${memoryId}`
                 },
                 payload => {
-                    console.log(payload)
                     switch (payload.eventType) {
                         case 'INSERT': {
                             const newMoment = payload.new
@@ -381,5 +389,55 @@ class LatestMoments {
     }
     private addMoments(newMoment: Moment): void {
         this.allMoments.push(newMoment)
+    }
+}
+
+class CountTime {
+    private readonly memoryDate: number
+    public constructor(memoryDate: string) {
+        this.memoryDate = Date.parse(memoryDate)
+    }
+
+    private getDiff(): number {
+        const now = new Date().getTime()
+        const memoryDate = new Date(this.memoryDate).getTime()
+        return now - memoryDate
+    }
+
+    public yearly(): string {
+        const diff = this.getDiff()
+        return (diff / (1000 * 60 * 60 * 24 * 365)).toFixed(2)
+    }
+
+    public monthly(): string {
+        const now = new Date()
+        const memoryDate = new Date(this.memoryDate)
+        const yearlyMonths = (now.getFullYear() - memoryDate.getFullYear()) * 12
+        return String(yearlyMonths + now.getMonth() - memoryDate.getMonth())
+    }
+
+    public weekly(): string {
+        const diff = this.getDiff()
+        return (diff / (1000 * 60 * 60 * 24 * 7)).toFixed(0)
+    }
+
+    public hourly(): string {
+        const diff = this.getDiff()
+        return (diff / (1000 * 60 * 60)).toFixed(0)
+    }
+
+    public daily(): string {
+        const diff = this.getDiff()
+        return (diff / (1000 * 60 * 60 * 24)).toFixed(0)
+    }
+
+    public minutes(): string {
+        const diff = this.getDiff()
+        return (diff / (1000 * 60)).toFixed(0)
+    }
+
+    public seconds(): string {
+        const diff = this.getDiff()
+        return (diff / 1000).toFixed(2)
     }
 }
