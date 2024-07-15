a7880697f0b451a95512632a8f7c8cbe9188a1b3
tom555-555 <tomok.development@gmail.com>
2024-07-14 20:21:48 -0700

    conflicts: resolved

--- src/api/memory.ts
+++ src/api/memory.ts
@@ -153,8 +153,9 @@ class MemoryApi {
         return !!res.error
     }
 
-    public async getAllCollaborators(memoryId: Collaborator['memoryId']): Promise<User[]> {
-        const res = await this.collaborators
+    public async getAllCollaborators(memoryId: Collaborator['memoryId']): PromiseMaybe<User[]> {
+        const res = await supabase
+            .from('collaborators')
             .select<string, CollaboratorJoinedUser>(
                 `
                     *,
@@ -210,9 +211,8 @@ class MemoryApi {
             .subscribe()
     }
 
-    // FIXME: here should not return the non-sense array value
     private async createMoment(MomentPayload: MomentPayload): PromiseMaybe<Moment[]> {
-        const res = await this.moments.insert(MomentPayload).select<string, Moment>()
+        const res = await supabase.from('moments').insert(MomentPayload).select<string, Moment>()
         return res.data
     }
 
@@ -249,24 +249,31 @@ class MemoryApi {
     }
 
     public async getAllMomentsByMemoryId(memoryId: Memory['id']): PromiseMaybe<Moment[]> {
-        const res = await this.moments.select<string, Moment>('*').eq<Moment['memoryId']>('memoryId', memoryId)
+        const res = await supabase
+            .from('moments')
+            .select<string, Moment>('*')
+            .eq<Moment['memoryId']>('memoryId', memoryId)
         if (!res.data) return
 
         return res.data
     }
 
-    // FIXME: update method not working correctly
     public async updateMoment(momentId: Moment['id'], payload: Partial<MomentPayload>): Promise<boolean> {
-        const res = await this.moments.update(payload).eq('id' satisfies keyof Moment, momentId)
+        const res = await supabase
+            .from('moments')
+            .update(payload)
+            .eq('id' satisfies keyof Moment, momentId)
         return !res.error
     }
 
     public async deleteMoments(momentIds: Moment['id'][]): PromiseMaybe<void> {
-        const res = await this.moments
-            .delete({ count: 'planned' })
+        console.log({ momentIds })
+        const res = await supabase
+            .from('moments')
+            .delete()
             .in('id' satisfies keyof Moment, momentIds)
             .select<string, Moment>()
-
+        console.log({ res })
         if (!res.data) return
 
         res.data.forEach(async moment => {
