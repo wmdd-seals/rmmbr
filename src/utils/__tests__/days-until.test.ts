87ac0d451196e7eb0ecb3d110314d2de205674b3
rasulomaroff <rasulomaroff@gmail.com>
2024-08-02 22:00:58 -0700

    spec(daysUntil): fixes the test expectations

--- src/utils/__tests__/days-until.test.ts
+++ src/utils/__tests__/days-until.test.ts
@@ -18,12 +18,12 @@ describe('hoursUntil', () => {
     it('should return 0 if the target date is current', () => {
         const date = new Date()
         vi.setSystemTime(date)
-        expect(daysUntil(new Date().toISOString())).toBe(0)
+        expect(daysUntil(new Date().toISOString())).toBe(1)
     })
     it('should return 0 if the target date is past', () => {
         const dstDate = '1800-01-01'
         const date = new Date('March 15, 2024, 04:00')
         vi.setSystemTime(date)
-        expect(daysUntil(dstDate)).toBe(0)
+        expect(daysUntil(dstDate)).toBe(1)
     })
 })
